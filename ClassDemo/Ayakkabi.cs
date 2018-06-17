18e56c0b118503e7c4ed599d02a3211d519f7d9e
umitliguler <info.umitguler@gmail.com>
2018-06-17 12:26:39 +0300

    first

---
+++ ClassDemo/Ayakkabi.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    class Ayakkabi
+    {
+        public AyakkabiTipi Tipi { get; set; }
+        public static string Malzeme;
+        public Ayakkabi()
+        {
+            Console.WriteLine("Ayakkabı nesnesi oluşturuldu");
+        }
+
+        ~Ayakkabi()
+        {
+            Console.WriteLine("Ayakkabı nesnesi bellekten atıldı.");
+        }
+
+        public Ayakkabi(string marka)
+        {
+            Marka = marka;
+        }
+        public double Fiyat { get; set; }
+
+        public void IndirimUygula(double indirimOrani)
+        {
+            Fiyat = Fiyat - (Fiyat * indirimOrani);
+        }
+
+        private int _numara;
+
+        public int Numara
+        {
+            get { return _numara; }
+            set
+            {
+                if (value > 0)
+                {
+                    _numara = value;
+                    Marka = "";
+                }
+                else
+                {
+                    _numara = 0;
+                }
+            }
+        }
+
+        public string Marka { get; set; }
+
+    }
+}
---
+++ ClassDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ayakkabi1 = new Ayakkabi("Kırmızı Kundura");
+            Console.WriteLine(ayakkabi1.Marka);
+            ayakkabi1.Numara = -70;
+
+
+
+            Ayakkabi.Malzeme = "Deri";
+
+            //Console.WriteLine(ayakkabi1.Numara);
+
+            //Console.WriteLine(Ayakkabi.Malzeme);
+
+            ayakkabi1.Numara = 39;
+
+            //Console.WriteLine(ayakkabi1.Numara);
+
+            ayakkabi1.Marka = "Mavi Kundura";
+            //Console.WriteLine(ayakkabi1.Marka);
+
+            ayakkabi1.Fiyat = 150;
+            //Console.WriteLine(ayakkabi1.Fiyat);
+
+            ayakkabi1.IndirimUygula(0.30);
+            //Console.WriteLine(ayakkabi1.Fiyat);
+
+            Ayakkabi.Malzeme = "Süet";
+            //Console.WriteLine(Ayakkabi.Malzeme);
+
+            ayakkabi1.Tipi = AyakkabiTipi.Kundura;
+
+            var ayakkabi2 = new Ayakkabi();
+            ayakkabi2.Tipi = AyakkabiTipi.Spor;
+
+            Console.WriteLine(ayakkabi1.Tipi);
+            Console.WriteLine(ayakkabi2.Tipi);
+
+
+            Console.ReadKey();
+        }
+    }
+}
